e13caca5d3ae501d0551076ed293f4c86b7ed135
blofeld80 <150074478+blofeld80@users.noreply.github.com>
2023-11-17 19:04:26 +0100

    Update 3_2_1_create_toolchain_file.sh

--- scripts/3_2_1_create_toolchain_file.sh
+++ scripts/3_2_1_create_toolchain_file.sh
@@ -115,4 +115,4 @@ sed -i "s;REPLACE_RTEMS_LIBBSD_INC_DIR;${RTEMS_LIBBSD_INSTALL_DIR}/${RTEMS_BSP_A
 sed -i "s;REPLACE_RTEMS_LIBBSD_LIB_DIR;${RTEMS_LIBBSD_INSTALL_DIR}/${RTEMS_BSP_ARCH}-rtems6/${RTEMS_BSP_NAME}/lib;g" ${CMAKE_TCF_DIR}/toolchain.cmake
 sed -i "s;REPLACE_RTEMS_INC_DIR;${RTEMS_OS_INSTALL_DIR}/${RTEMS_BSP_ARCH}-rtems6/${RTEMS_BSP_NAME}/lib/include;g" ${CMAKE_TCF_DIR}/toolchain.cmake
 sed -i "s;REPLACE_RTEMS_LIB_DIR;${RTEMS_OS_INSTALL_DIR}/${RTEMS_BSP_ARCH}-rtems6/${RTEMS_BSP_NAME}/lib;g" ${CMAKE_TCF_DIR}/toolchain.cmake
-sed -i "s;REPLACE_APP_FOLDER;${THIS_DIR}/app;g" ${CMAKE_TCF_DIR}/toolchain.cmake
+sed -i "s;REPLACE_APP_FOLDER;${THIS_DIR}/../app;g" ${CMAKE_TCF_DIR}/toolchain.cmake
