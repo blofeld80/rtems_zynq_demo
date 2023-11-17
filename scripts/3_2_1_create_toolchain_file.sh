816a24c52dd2c89b0b659f40688c70a94510e252
blofeld80 <150074478+blofeld80@users.noreply.github.com>
2023-11-17 18:24:20 +0100

    Update 3_2_1_create_toolchain_file.sh

--- scripts/3_2_1_create_toolchain_file.sh
+++ scripts/3_2_1_create_toolchain_file.sh
@@ -6,10 +6,97 @@
 THIS_SCRIPT=$(realpath "$0")
 THIS_DIR=$(dirname "${THIS_SCRIPT}")
 
+#################################################
+## check the arguments the user has passed
+function print_help {
+  echo "Please call this script using ./3_2_1_create_toolchain_file -b <bsp_name>"
+  echo "where bsp_name is one of:"
+  echo "  xilinx_zynq_pynq"
+  echo "  xilinx_zynq_picozed"
+  echo "  xilinx_zynq_microzed"
+  echo "  xilinx_zynq_zybo"
+  echo "  xilinx_zynq_zybo_z7"
+  echo "  xilinx_zynq_zedboard"
+  echo "  xilinx_zynq_zc702"
+  echo "  xilinx_zynq_zc706"
+}
+
+function print_error {
+  echo "ERROR: ${1}"
+}
+
+
+if [[ $# -eq 0 ]] ; then
+    print_error "No board provided!"
+    print_help
+    exit 1
+fi
+
+for a in "$@"; do
+  case $1 in
+    -b|--board)
+      RTEMS_BSP_NAME="${2}"
+      shift
+      shift
+      ;;
+    -h|--help)
+      print_help
+      exit 1
+      ;;
+    -*|--*)
+      echo "Unknown argument $1"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+
+
+#################################################
+## check if the board is supported by this script
+case ${RTEMS_BSP_NAME} in
+
+  xilinx_zynq_pynq)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_picozed)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_microzed)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zybo)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zybo_z7)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zedboard)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zc702)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zc702)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  xilinx_zynq_zc706)
+    echo "Builing for bsp: ${RTEMS_BSP_NAME}"
+    ;;
+  *)
+    print_error "Unknown board ${RTEMS_BSP_NAME}"
+    exit 1
+    ;;
+esac
+
+
+  
 # Read the configuration
 source ${THIS_DIR}/config/config.sh
 
-CMAKE_TCF_DIR=${TOP_INSTALL_DIR}/cmake
+CMAKE_TCF_DIR=${RTEMS_OS_INSTALL_DIR}/../cmake
 
 if [ -d "${CMAKE_TCF_DIR}" ]; then 
   rm -rf ${CMAKE_TCF_DIR}
@@ -17,7 +104,8 @@ fi
 
 mkdir ${CMAKE_TCF_DIR}
 
-cp ${THIS_DIR}/app/cmake/toolchain.cmake.in ${CMAKE_TCF_DIR}/toolchain.cmake
+
+cp ${THIS_DIR}/../app/cmake/toolchain.cmake.in ${CMAKE_TCF_DIR}/toolchain.cmake
 
 
 sed -i "s;REPLACE_RTEMS_TOOLCHAIN_INSTALL_DIR;${RTEMS_TOOLCHAIN_INSTALL_DIR};g" ${CMAKE_TCF_DIR}/toolchain.cmake
